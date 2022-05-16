ddfba199764338a6497b6648cac0fc9b0907e764
aziz403 <azizbenmallouk4@gmail.com>
2022-05-16 11:55:23 +0100

    show article action

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,30 +9,18 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'app_default')]
+    #[Route('/', name: 'article_index')]
     public function index(HttpClientInterface $client): Response
     {
         $response = $client->request('GET','https://www.bbc.com/arabic/topics/c719d2ely7xt');
 
-
         $html = $response->getContent();
 
-        $content = $this->HTMLToObj($html);
-        dump($content);
-        exit();
-
-
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
-
-    function HTMLToObj($html) {
         $dom = new \DOMDocument();
         @$dom->loadHTML($html);
 
         foreach($dom->getElementsByTagName('article') as $el){
-            $result[] = [
+            $content[] = [
                 'photo'=>$el->getElementsByTagName('img')?->item(0)?->getAttribute('src'),
                 'title'=>$el->getElementsByTagName('a')?->item(0)?->getElementsByTagName('span')->item(0)->textContent,
                 'body'=>$el->getElementsByTagName('div')?->item(3)->getElementsByTagName('p')->item(0)->textContent,
@@ -41,6 +29,30 @@ class DefaultController extends AbstractController
             ];
         }
 
-        return $result;
+        return $this->json($content);
+    }
+
+
+    #[Route('/{link}', name: 'article_show')]
+    public function show(HttpClientInterface $client,$link): Response
+    {
+        $url = 'https://www.bbc.com/arabic/'.$link;
+        $response = $client->request('GET',$url);
+
+        $html = $response->getContent();
+
+        $dom = new \DOMDocument();
+        @$dom->loadHTML($html);
+
+        $main = $dom->getElementsByTagName('main')->item(0);
+
+        $content = [
+            'photo'=>$main->getElementsByTagName('img')?->item(0)?->getAttribute('src'),
+            'title'=>$main->getElementsByTagName('h1')?->item(0)->textContent,
+            'time'=>$main->getElementsByTagName('time')?->item(0)->textContent,
+            'link'=>$url,
+        ];
+
+        return $this->json($content);
     }
 }
