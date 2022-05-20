96d41576348a19bcbb8b0a330d743753b7991028
aziz403 <azizbenmallouk4@gmail.com>
2022-05-20 12:43:16 +0100

    add filtre in read articles

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -10,23 +11,31 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'article_index')]
-    public function index(HttpClientInterface $client): Response
+    public function index(HttpClientInterface $client,Request $request): Response
     {
-        $response = $client->request('GET','https://www.bbc.com/arabic/topics/c719d2ely7xt');
+        $bbcUrl = 'https://www.bbc.com/arabic/';
+        $topic = 'topics/c719d2ely7xt';
+        $page = '/page/';
+        $start = $request->get('start') ?? 1;
+        $end = $request->get('end') ??  1;
 
-        $html = $response->getContent();
+        for ($i = $start;$i<=$end;$i++){
+            $response = $client->request('GET',$bbcUrl.$topic.$page.$i);
 
-        $dom = new \DOMDocument();
-        @$dom->loadHTML($html);
+            $html = $response->getContent();
 
-        foreach($dom->getElementsByTagName('article') as $el){
-            $content[] = [
-                'photo'=>$el->getElementsByTagName('img')?->item(0)?->getAttribute('src'),
-                'title'=>$el->getElementsByTagName('a')?->item(0)?->getElementsByTagName('span')->item(0)->textContent,
-                'body'=>$el->getElementsByTagName('div')?->item(3)->getElementsByTagName('p')->item(0)->textContent,
-                'time'=>$el->getElementsByTagName('time')->item(0)->getElementsByTagName('span')?->item(1)->textContent,
-                'link'=>$el->getElementsByTagName('a')?->item(0)->getAttribute('href'),
-            ];
+            $dom = new \DOMDocument();
+            @$dom->loadHTML($html);
+
+            foreach($dom->getElementsByTagName('article') as $el){
+                $content[] = [
+                    'photo'=>$el->getElementsByTagName('img')?->item(0)?->getAttribute('src'),
+                    'title'=>$el->getElementsByTagName('a')?->item(0)?->getElementsByTagName('span')->item(0)?->textContent,
+                    'body'=>$el->getElementsByTagName('div')?->item(3)->getElementsByTagName('p')->item(0)?->textContent,
+                    'time'=>$el->getElementsByTagName('time')->item(0)->getElementsByTagName('span')?->item(1)?->textContent,
+                    'link'=>$el->getElementsByTagName('a')?->item(0)->getAttribute('href'),
+                ];
+            }
         }
 
         return $this->json($content);
